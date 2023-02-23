ddc7637ef892c444a4520494b2ec397ea74a3f1b
thimooura <t.695@hotmail.com>
2023-02-23 14:18:58 -0300

    implementa api de teste

--- src/main/java/br/com/desafio/catalogo/CatalogoApplication.java
+++ src/main/java/br/com/desafio/catalogo/CatalogoApplication.java
@@ -2,10 +2,18 @@ package br.com.desafio.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CatalogoApplication {
-
+	@GetMapping
+		public String getHomeTeste(){
+			return "Catalogo";
+		}
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
 	}
