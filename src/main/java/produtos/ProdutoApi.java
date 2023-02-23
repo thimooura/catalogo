11dca06c477f7b68dce1cd37a448822e5e5ab241
thimooura <t.695@hotmail.com>
2023-02-23 18:25:26 -0300

    implementa contrato da api

---
+++ src/main/java/produtos/ProdutoApi.java
@@ -0,0 +1,12 @@
+package produtos;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/vi/produto")
+public interface ProdutoApi {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    ProdutoResponse postProduto(@RequestBody ProdutoRequest produtoRequest);
+}
